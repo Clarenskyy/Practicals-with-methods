4928aa8d0ad96e6d777e4dbb7214b15c033bcf20
Clarenskyy <150917878+Clarenskyy@users.noreply.github.com>
2024-06-12 13:45:29 +0800

    Update Program.cs

--- Practical_3/Program.cs
+++ Practical_3/Program.cs
@@ -39,24 +39,18 @@ namespace Practical_3
             //determines the age through simple math equation
             int age = 2024 - birthyear;
 
-            //compare the birthyear through different conditional statements to determin the generation
+            //compare the birthyear through different conditional statements to determine the generation
+            GenerationDeterminer(birthyear, age);
+        }
+        public static void GenerationDeterminer(int birthyear, int age)
+        {
             if (birthyear < 1928)
             {
                 Console.WriteLine($"{age} year/s old, and Late Adulthood Ages. Unfortunately we don't have enough data to determine your generation");
             }
             else if (birthyear >= 1928 && birthyear <= 1945)
             {
-                Console.WriteLine($"{age} year/s old, The Silent Generation, ");
-
-                //compares age to a specific ages to determine the life stages
-                if (age > 80)
-                {
-                    Console.Write("Late Adulthood ages");
-                }
-                else
-                {
-                    Console.Write("Mature Adulthood ages");
-                }
+                AgeStagesDeterminerTheSilentGeneration(age);
             }
             else if (birthyear >= 1946 && birthyear <= 1964)
             {
@@ -64,66 +58,95 @@ namespace Practical_3
             }
             else if (birthyear >= 1965 && birthyear <= 1980)
             {
-                Console.WriteLine($"{age} year/s old, Gen X, ");
-                if (age > 50)
-                {
-                    Console.Write("Mature Adulthood ages");
-                }
-                else
-                {
-                    Console.Write("Midlife ages");
-                }
+                AgeStagesDeterminerGenX(age);
             }
             else if (birthyear >= 1981 && birthyear <= 1996)
             {
-                Console.WriteLine($"{age} year/s old, Millenials, ");
-                if (age > 35)
-                {
-                    Console.Write("Midlife ages");
-                }
-                else
-                {
-                    Console.Write("Early Adulthood ages");
-                }
+                AgeStagesDeterminerMillenials(age);
             }
             else if (birthyear >= 1997 && birthyear <= 2012)
             {
-                Console.WriteLine($"{age} year/s old, Gen Z, ");
-                if (age > 21)
-                {
-                    Console.Write("Early Adulthood ages");
-                }
-                else
-                {
-                    Console.Write("Adolescence ages");
-                }
+                AgeStagesDeterminerGenZ1(age);
             }
             else if (birthyear >= 1997 && birthyear <= 2012)
             {
-                Console.WriteLine($"{age} year/s old, Gen Z, ");
-                if (age > 9)
-                {
-                    Console.Write("Late Childhood ages");
-                }
-                else if (age > 6)
-                {
-                    Console.Write("Middle Childhood ages");
-                }
-                else if (age > 3)
-                {
-                    Console.Write("Early Childhood ages");
-                }
-                else if (age > 0)
-                {
-                    Console.Write("Infant ages");
-                }
+                AgeStagesDeterminerGenZ2(age);
+
             }
             else if (birthyear > 2024)
             {
                 Console.WriteLine("How are you here? you're not born yet :o");
             }
+        }
+        public static void AgeStagesDeterminerTheSilentGeneration(int age)
+        {
+            Console.WriteLine($"{age} year/s old, The Silent Generation, ");
 
-
+            //compares age to a specific ages to determine the life stages
+            if (age > 80)
+            {
+                Console.Write("Late Adulthood ages");
+            }
+            else
+            {
+                Console.Write("Mature Adulthood ages");
+            }
+        }
+        public static void AgeStagesDeterminerGenX(int age)
+        {
+            Console.WriteLine($"{age} year/s old, Gen X, ");
+            if (age > 50)
+            {
+                Console.Write("Mature Adulthood ages");
+            }
+            else
+            {
+                Console.Write("Midlife ages");
+            }
+        }
+        public static void AgeStagesDeterminerMillenials(int age)
+        {
+            Console.WriteLine($"{age} year/s old, Millenials, ");
+            if (age > 35)
+            {
+                Console.Write("Midlife ages");
+            }
+            else
+            {
+                Console.Write("Early Adulthood ages");
+            }
+        }
+        public static void AgeStagesDeterminerGenZ1(int age)
+        {
+            Console.WriteLine($"{age} year/s old, Gen Z, ");
+            if (age > 21)
+            {
+                Console.Write("Early Adulthood ages");
+            }
+            else
+            {
+                Console.Write("Adolescence ages");
+            }
+        }
+        public static void AgeStagesDeterminerGenZ2(int age)
+        {
+            Console.WriteLine($"{age} year/s old, Gen Z, ");
+            if (age > 9)
+            {
+                Console.Write("Late Childhood ages");
+            }
+            else if (age > 6)
+            {
+                Console.Write("Middle Childhood ages");
+            }
+            else if (age > 3)
+            {
+                Console.Write("Early Childhood ages");
+            }
+            else if (age > 0)
+            {
+                Console.Write("Infant ages");
+            }
         }
     }
 }
